c81aa25062e9cee8a2c66ba57bcfc74909f7e214
Devansh Jaiswal <youremail@example.com>
2026-04-27 01:18:22 +0530

    chore: adjust eslint rules to demote strict typing to warnings

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -5,6 +5,16 @@ import nextTs from "eslint-config-next/typescript";
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
+  {
+    rules: {
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-unused-vars": "warn",
+      "@next/next/no-img-element": "warn",
+      "react/no-unescaped-entities": "warn",
+      "@typescript-eslint/no-empty-object-type": "warn",
+      "prefer-const": "warn"
+    }
+  },
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
